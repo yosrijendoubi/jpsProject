d8b0f7e5a5f6980401112f454dadb27d0bc3c7e6
TaherZ <taher.zoghlami1b@gmail.com>
2020-07-27 15:31:36 +0100

    ttt

--- src/AppBundle/Entity/Employe.php
+++ src/AppBundle/Entity/Employe.php
@@ -3,12 +3,18 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Employe
  *
  * @ORM\Table(name="employe")
  * @ORM\Entity
+ * @Vich\Uploadable
  */
 class Employe
 {
@@ -106,6 +112,9 @@ class Employe
      */
     private $tel;
 
+
+
+
     /**
      * @return \Marche
      */
@@ -137,5 +146,154 @@ class Employe
      * })
      */
     private $idMarche;
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="employe", fileNameProperty="imageName")
+     * @var File
+     */
+    private $imageFile;
+
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string|null
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTimeInterface|null
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="RIB", type="string", length=50, nullable=false)
+     */
+    private $RIB;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="NumCNSS", type="string", length=50, nullable=false)
+     */
+    private $NumCNSS;
+
+    /**
+     * @var TextareaType
+     *
+     * @ORM\Column(name="Vetements", type="string", length=200, nullable=false)
+     */
+    private $Vetements;
+
+
+
+
+    /**
+     * @return string|null
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * @param string|null $imageName
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+    public function setImageFile(File $imageFile = null)
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRIB()
+    {
+        return $this->RIB;
+    }
+
+    /**
+     * @param string $RIB
+     */
+    public function setRIB($RIB)
+    {
+        $this->RIB = $RIB;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumCNSS()
+    {
+        return $this->NumCNSS;
+    }
+
+    /**
+     * @param string $NumCNSS
+     */
+    public function setNumCNSS($NumCNSS)
+    {
+        $this->NumCNSS = $NumCNSS;
+    }
+
+    /**
+     * @return TextareaType
+     */
+    public function getVetements()
+    {
+        return $this->Vetements;
+    }
+
+    /**
+     * @param TextareaType $Vetements
+     */
+    public function setVetements($Vetements)
+    {
+        $this->Vetements = $Vetements;
+    }
+
+
+
+
+
+
 }
 
--- src/AppBundle/Form/EmployeType.php
+++ src/AppBundle/Form/EmployeType.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class EmployeType extends AbstractType
 {
@@ -13,7 +15,7 @@ class EmployeType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('prenom')->add('tel')->add('idMarche');
+        $builder->add('nom')->add('prenom')->add('tel')->add('RIB')->add('NumCNSS')->add('Vetements', TextareaType::class)->add('imageFile',VichImageType::class)->add('idMarche');
     }/**
      * {@inheritdoc}
      */
